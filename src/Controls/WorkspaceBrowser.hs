a6b3421460c42c583b883a68ee82ebbad0491e11
Chris Van Vranken <cessationoftime@gmail.com>
2014-12-07 22:49:12 -0500

    .

--- src/Controls/WorkspaceBrowser.hs
+++ src/Controls/WorkspaceBrowser.hs
@@ -69,14 +69,18 @@ createWorkspaceBrowser frame1 inp setupIO = do
     eOpenWorkspaceOk <- fileDialogOkEvent Open "" [Workspace] frame1 eButtonOpenWS
     eCreateProjectOk <- fileDialogOkEvent New "NewProject.n6proj" [Project] frame1 eButtonCreateWS
     liftIO $ setupIO $ WorkspaceBrowser panel
-    --wbDataIO <- liftIO $ return $ return wbData
-    (eventUpdateWorkspaceData,eventUpdateWorkspaceDataInput) <- Frame.newEvent -- create newEvent to use to break the cyclical dependency between "loader" and the workspaceData behavior
+    -- create newEvent to use to break the cyclical dependency between "loader" and the workspaceData behavior. This can be though of as a "helical dependency" since it is still dependent on the previous value in time
+    (eventUpdateWorkspaceData,eventUpdateWorkspaceDataInput) <- Frame.newEvent
+
+    -- track workspaceData as a behavior, run IO and use output to update the behavior.
     let workspaceDataBehavior = stepper wbData eventUpdateWorkspaceData
         wbInput = WorkspaceBrowserInput eCreateWorkspaceOk eOpenWorkspaceOk eCreateProjectOk
         inputs =  unite $ wbInput:inp
         loadW  =  (loadWorkspace <$> workspaceDataBehavior) <@> (eCreateWorkspaceOk `union` eOpenWorkspaceOk)
         loadP = (loadProject <$> workspaceDataBehavior) <@> eCreateProjectOk
         loader = loadW `union` loadP
+
+    -- send event to update the workspaceDataBehavior
     reactimate $ (\wbdIO -> wbdIO >>= eventUpdateWorkspaceDataInput) <$> loader -- update the behavior with the new workSpaceData using a reactimate and eventZInput
 
     return (WorkspaceBrowserOutputs 0)
